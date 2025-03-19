7b7cb6ca783c4cc54b8f90bef5a959b594a4b4ff
qorlgns1 <rlgns0610@gmail.com>
2025-03-19 18:53:34 +0900

    initial commit

---
+++ src/main/java/com/soon_my_room/soon_my_room/SoonMyRoomApplication.java
@@ -0,0 +1,13 @@
+package com.soon_my_room.soon_my_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoonMyRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoonMyRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soon_my_room/soon_my_room/SoonMyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soon_my_room.soon_my_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoonMyRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
