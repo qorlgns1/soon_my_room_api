c1b5592cef138ab84ce6be5dc788001b3b5b08f8
qorlgns1 <rlgns0610@gmail.com>
2025-03-23 10:04:38 +0900

    hotfix: swagger cors error

--- src/main/java/com/soon_my_room/soon_my_room/SoonMyRoomApplication.java
+++ src/main/java/com/soon_my_room/soon_my_room/SoonMyRoomApplication.java
@@ -1,9 +1,12 @@
 package com.soon_my_room.soon_my_room;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class SoonMyRoomApplication {
 
   public static void main(String[] args) {
