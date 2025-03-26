efd8fdd273294c2876f02322b644f085622a42ad
qorlgns1 <rlgns0610@gmail.com>
2025-03-26 15:31:48 +0900

    fix: 스웨거 기본 경로 설정

--- src/main/java/com/soon_my_room/soon_my_room/config/OpenApiConfig.java
+++ src/main/java/com/soon_my_room/soon_my_room/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -35,6 +36,7 @@ public class OpenApiConfig {
     return new OpenAPI()
         .info(info)
         .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-        .addSecurityItem(securityRequirement);
+        .addSecurityItem(securityRequirement)
+        .addServersItem(new Server().url("/"));
   }
 }
