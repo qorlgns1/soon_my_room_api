bbc13273888850fd79291c1bab67312b78bfc0a3
qorlgns1 <rlgns0610@gmail.com>
2025-04-04 21:41:14 +0900

    fix: 테스트에서 H2 사용 명시 (기본값은 ANY)

--- src/test/java/com/soon_my_room/soon_my_room/SoonMyRoomApplicationTests.java
+++ src/test/java/com/soon_my_room/soon_my_room/SoonMyRoomApplicationTests.java
@@ -1,9 +1,11 @@
 package com.soon_my_room.soon_my_room;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY) // H2 사용 명시 (기본값은 ANY)
 class SoonMyRoomApplicationTests {
 
   @Test
