eed4162942e7dfe7365d97968aa8b69382abe2d7
qorlgns1 <rlgns0610@gmail.com>
2025-04-04 22:33:38 +0900

    fix: 테스트시 application-test.properties 사용 명시

--- src/test/java/com/soon_my_room/soon_my_room/SoonMyRoomApplicationTests.java
+++ src/test/java/com/soon_my_room/soon_my_room/SoonMyRoomApplicationTests.java
@@ -3,9 +3,11 @@ package com.soon_my_room.soon_my_room;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY) // H2 사용 명시 (기본값은 ANY)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class SoonMyRoomApplicationTests {
 
   @Test
