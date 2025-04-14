2c32675273f606aaf1fbc7221f6b2cc0ccef4572
qorlgns1 <rlgns0610@gmail.com>
2025-04-15 05:22:05 +0900

    refactor: spotlessApply

--- src/main/java/com/soon_my_room/soon_my_room/controller/CommentController.java
+++ src/main/java/com/soon_my_room/soon_my_room/controller/CommentController.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
